10e1585cc02cc9108b5b14db25ff517cc73ea3e6
Tristan Rivoallan <tristan@rivoallan.net>
2008-09-11 15:49:04 +0200

    added getCliCommandSpec to interface

--- src/File/CSV/Converter/Strategy.php
+++ src/File/CSV/Converter/Strategy.php
@@ -8,4 +8,6 @@ interface File_CSV_Converter_Strategy
   public function __construct(array $params = array());
 
   public function convert(array $data, $destinationfile_url);
+
+  public static function getCliCommandSpecification();
 }
